a7f24ce2b8a682d6467e0baa17e193c2a7bd4025
chinmaykodag <30663316+chinmaykodag@users.noreply.github.com>
2017-08-16 00:02:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,9 +3,9 @@ console.log('Loaded!');
 var img=document.getElementById('eesha');
 marginLeft=0;
 function moveRight(){
-    marginLeft=marginLeft + 10;
+    marginLeft=marginLeft + 5;
     img.style.marginLeft=marginLeft + 'px';
 }
 img.onclick=function (){
-    var interval=setInterval(moveRight,1);
+    var interval=setInterval(moveRight,50);
 };
